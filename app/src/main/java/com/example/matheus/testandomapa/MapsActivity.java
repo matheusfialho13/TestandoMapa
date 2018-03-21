2aef060d7bbe4bc935d37fee6d5a5d908b8d474a
Matheus F <matheus.fialho13@hotmail.com>
2018-03-21 14:22:39 -0300

    Da zoom e mostra Lat. Long ao clicar. Não tem toolbar ou barra de opções.

---
+++ app/src/main/java/com/example/matheus/testandomapa/MapsActivity.java
@@ -0,0 +1,136 @@
+package com.example.matheus.testandomapa;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Date;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        LocationListener, GoogleMap.OnMapClickListener { //
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
+    private Marker currentLocationMaker;
+    private LatLng currentLocationLatLong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setOnMapClickListener(this);
+
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+        //mMap.getUiSettings().setMapToolbarEnabled(true);
+        //mMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        // Add a marker in Sydney and move the camera
+        LatLng SantaCruz = new LatLng(-06.2315, -36.02);
+
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(SantaCruz);
+        marker.title("Santinha");
+        mMap.addMarker(marker);
+
+        mMap.addMarker(new MarkerOptions().position(SantaCruz).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(SantaCruz));
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+        Toast.makeText(getApplicationContext(), "Coordenadas"+latLng.toString(),
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    // Métodos do LocationListener
+    @Override
+    public void onLocationChanged(Location location) {
+        if (currentLocationMaker != null) {
+            currentLocationMaker.remove();
+        }
+        //Add marker
+        currentLocationLatLong = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(currentLocationLatLong);
+        markerOptions.title("Localização atual");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+        currentLocationMaker = mMap.addMarker(markerOptions);
+
+        //Move to new location
+        CameraPosition cameraPosition = new CameraPosition.Builder().zoom(15).target(currentLocationLatLong).build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        //LocationData locationData = new LocationData(location.getLatitude(), location.getLongitude());
+        //mDatabase.child("location").child(String.valueOf(new Date().getTime())).setValue(locationData);
+
+        Toast.makeText(this, "Localização atualizada", Toast.LENGTH_SHORT).show();
+        //getMarkers();
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
